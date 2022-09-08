247bf426c7436e400cce5edd5b76eff4f7bee26c
NEARworld <artemismars2@outlook.com>
2022-09-08 23:55:37 +0900

    chore: initial webpack & babel setup

---
+++ src/App.jsx
@@ -0,0 +1,5 @@
+function App() {
+  return <div>Hello I am App.jsx</div>;
+}
+
+export default App;
---
+++ webpack.config.js
@@ -0,0 +1,29 @@
+const path = require("path");
+
+module.exports = {
+  mode: "development",
+  entry: "./index.js",
+  output: {
+    filename: "main.js",
+    path: path.resolve(__dirname, "build"),
+  },
+  resolve: {
+    extensions: [".js", ".jsx", ".json"],
+  },
+  target: "web",
+  devServer: {
+    static: ["./public"],
+    open: true,
+    hot: true,
+    liveReload: true,
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)/,
+        exclude: "/node_modules",
+        use: "babel-loader",
+      },
+    ],
+  },
+};
