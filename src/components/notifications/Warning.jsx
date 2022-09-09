041f51af336c6e28dd661fbad570ade4f36be55b
NEARworld <artemismars2@outlook.com>
2022-09-09 23:18:56 +0900

    refactor(warning): add custom class to WarningIcon

--- src/components/notifications/Warning.jsx
+++ src/components/notifications/Warning.jsx
@@ -1,9 +1,10 @@
 import WarningIcon from '../icons/WarningIcon';
+import '../../styles/warning.scss';
 
 function Warning({ content }) {
   return (
     <div className="warning-container">
-      <WarningIcon />
+      <WarningIcon customClass="warning-icon-survey" />
       <p className="warning-text">{content}</p>
     </div>
   );
