024d578c8de2ef0bb1fc55b3ae33b0107f6eb91c
NEARworld <artemismars2@outlook.com>
2022-09-10 02:27:37 +0900

    feat(reasons): customizing radio button

--- src/pages/Main/Survey/Body/Reasons/index.jsx
+++ src/pages/Main/Survey/Body/Reasons/index.jsx
@@ -1,13 +1,29 @@
 import reasonsData from 'Mocks/reasonsData';
+import { useState } from 'react';
 
 function Reasons() {
+  const [checked, setChecked] = useState(0);
   return (
     <div className="reason-container">
       {reasonsData.map((item) => (
-        <div key={item.id} className="reason-wrapper">
-          <input type="radio" name="reason" value={item.data} />
-          <label htmlFor="html">{item.data}</label>
-        </div>
+        <label
+          className="reason"
+          key={item.id}
+          htmlFor={item.id}
+          style={{
+            backgroundColor: checked === item.id ? '#d5e3fe' : 'transparent',
+          }}
+        >
+          <input
+            type="radio"
+            name="reason"
+            value={item.data}
+            id={item.id}
+            onChange={() => setChecked(item.id)}
+          />
+          <span className="checkmark"></span>
+          <span className="reason-text">{item.data}</span>
+        </label>
       ))}
     </div>
   );
