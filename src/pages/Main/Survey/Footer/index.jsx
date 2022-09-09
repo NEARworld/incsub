e4712efe97e818d575b052171ef5b5d473cad99b
NEARworld <artemismars2@outlook.com>
2022-09-09 23:20:26 +0900

    refactor(survey-footer): added content to Warning

--- src/pages/Main/Survey/Footer/index.jsx
+++ src/pages/Main/Survey/Footer/index.jsx
@@ -1,7 +1,11 @@
+import Warning from '../../../../components/notifications/Warning';
+
 function Footer() {
+  const content =
+    "Are you absolutely sure you want to cancel? If you do you'll lose access to so many cool things. And you'll lose the chance to stay on this plan at the same cost ongoing (regardless of future price increases).";
   return (
     <footer>
-      <div className="warning"></div>
+      <Warning content={content} />
       <div className="survey-control">
         <button>Back</button>
         <button>Cancel Membership</button>
