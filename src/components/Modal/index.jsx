e76df90c9f5ce5355ff42a794d0195ff11341757
NEARworld <artemismars2@outlook.com>
2022-09-10 13:49:42 +0900

    style(modal): replaced styles to module css in Modal component

--- src/components/Modal/index.jsx
+++ src/components/Modal/index.jsx
@@ -1,4 +1,4 @@
-import 'Styles/modal.scss';
+import styles from 'Styles/modal.module.scss';
 import CloseButton from 'Buttons/CloseButton';
 import PrimaryButton from 'Buttons/PrimaryButton';
 import DiscountIcon from 'Icons/DiscountIcon';
@@ -8,44 +8,44 @@ function Modal({ modal, survey }) {
   const { setIsSurveyOpen } = survey;
   return (
     <div
-      className="modal-wrapper"
+      className={styles.wrapper}
       style={{
         display: isModalOpen ? 'block' : 'none',
       }}
     >
       <CloseButton
-        customWrapper="modal-close-wrapper"
-        customClass="modal-close-btn"
+        customWrapper={styles.close_wrapper}
+        customClass={styles.close_btn}
         trigger={setIsModalOpen}
       />
-      <div className="modal-body-container">
-        <DiscountIcon customWrapper="modal-discount-icon-wrapper" />
-        <div className="modal-body">
-          <div className="modal-title">
+      <div className={styles.body_container}>
+        <DiscountIcon customWrapper={styles.discount_icon_wrapper} />
+        <div className={styles.body}>
+          <div className={styles.title}>
             Before You Go... <br />
             Would 50% Off For 6 Months Help?
           </div>
-          <div className="modal-content">
-            <p className="modal-content-item-1">
+          <div className={styles.content}>
+            <p className={styles.content_item_1}>
               We know things REALLY suck in the world right now - and many
               businesses and freelancers are struggling to stay afloat during
               this COVID-19 crisis.
             </p>
-            <div className="modal-content-item-2">
-              <p className="modal-content-item-2-1">
+            <div className={styles.content_item_2}>
+              <p className={styles.content_item_2_1}>
                 So if you could use the extra cushion, grab 50% off for 6
                 months.
               </p>
-              <p className="modal-content-item-2-2">
+              <p className={styles.content_item_2_2}>
                 We hope you're staying safe and healthy!
               </p>
-              <div className="modal-content-control">
+              <div className={styles.content_control}>
                 <PrimaryButton
-                  customClass="modal-btn-primary"
+                  customClass="btn_primary"
                   text="50 % OFF For 6 Months"
                 />
                 <button
-                  className="modal-cancel"
+                  className={styles.cancel}
                   onClick={() => {
                     setIsModalOpen(false);
                     setIsSurveyOpen(true);
