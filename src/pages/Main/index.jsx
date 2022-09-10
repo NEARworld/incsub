97740987d8a8ffd4d2f73ae89ccee5d8aae72df6
NEARworld <artemismars2@outlook.com>
2022-09-10 13:56:07 +0900

    style(Main): style to CSS module

--- src/pages/Main/index.jsx
+++ src/pages/Main/index.jsx
@@ -1,13 +1,13 @@
 import { useState } from 'react';
 import Modal from 'Components/Modal';
 import Survey from './Survey';
-import 'Styles/main.scss';
+import styles from 'Styles/main.module.scss';
 
 function Main() {
   const [isSurveyOpen, setIsSurveyOpen] = useState(false);
   const [isModalOpen, setIsModalOpen] = useState(true);
   return (
-    <div className="main-wrapper">
+    <div className={styles.wrapper}>
       {!isSurveyOpen ? (
         <Modal
           modal={{ isModalOpen, setIsModalOpen }}
