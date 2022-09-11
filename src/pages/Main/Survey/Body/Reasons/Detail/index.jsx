3bf7dded15ae7fbe1371b172acecb3a91e9d0aa1
NEARworld <artemismars2@outlook.com>
2022-09-11 22:17:56 +0900

    chore(Detail): remove unncessary useEffect

--- src/pages/Main/Survey/Body/Reasons/Detail/index.jsx
+++ src/pages/Main/Survey/Body/Reasons/Detail/index.jsx
@@ -1,11 +1,7 @@
-import { useEffect } from 'react';
 import styles from 'Styles/survey/detail.module.scss';
 import detailGenerator from './detailGenerator';
 
 function Detail({ checked, detailRef }) {
-  useEffect(() => {
-    console.log('Detail', checked);
-  });
   return detailGenerator(styles, checked, detailRef);
 }
 
